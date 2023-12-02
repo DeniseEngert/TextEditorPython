4af02086b1c6d4398fa05f6bc04bd8cf620245c8
DeniseEngert <10726000+DeniseEngert@users.noreply.github.com>
2023-12-02 02:06:34 +0100

    Rework Frame

--- texteditor.py
+++ texteditor.py
@@ -4,41 +4,80 @@ import tkinter.filedialog
 
 # Editor runs with Python3 only
 
-root = Tk("Text Editor")
+### Variables ###
+root = Tk()
+root.title("Text Editor")
+root.geometry("1000x600")  # size of the frame
+root.resizable(True, True)
+
+global open_status_name
+open_status_name = False
+
+global selected
+selected = False
+
+# add Toolbar
+toolbar_frame = Frame(root)
+toolbar_frame.pack(fill=X, pady=5)
+
+# add mainframe
+my_frame = Frame(root)
+my_frame.pack(pady=5)
+
+# add scrollbars
+vertscroll = Scrollbar(my_frame)
+vertscroll.pack(side=RIGHT, fill=Y)
+horscroll = Scrollbar(my_frame, orient="horizontal")
+horscroll.pack(side=BOTTOM, fill=X)
 
 # add a text box
-text = Text(root)
-text.grid()
+text = Text(my_frame, width=80, height=20, font=("Helvetica", 16), selectbackground="lightgrey",
+            selectforeground="black", undo=True, yscrollcommand=vertscroll.set, xscrollcommand=horscroll.set, wrap="none")
+text.pack()
 
+# configure scrollbar
+vertscroll.config(command=text.yview)
+horscroll.config(command=text.xview)
 
-# add a button to save the text
-def saveas():
-    global text
-    t = text.get("1.0", "end-1c")
-    savelocation = tkinter.filedialog.asksaveasfilename()
-    file1 = open(savelocation, "w+")
-    file1.write(t)
-    file1.close()
+# add a menu
+mymenu = Menu(root)
+root.config(menu=mymenu)
 
+# add status bar to buttom
+statusbar = Label(root, text="Ready       ", anchor=E)
+statusbar.pack(fill=X, side=BOTTOM, ipady=15)
 
-button = Button(root, text="Save", command=saveas)
-button.grid()
+##### Filemenu ##### ToDo
 
-
-# add font changer
-def changefont(font):
-    global text
-    text.config(font=font)
-
-
-font = Menubutton(root, text="Font")
-font.grid()
-font.menu = Menu(font, tearoff=0)
-font["menu"] = font.menu
-Helvetica = IntVar()
-Courier = IntVar()
-font.menu.add_checkbutton(label="Courier", variable=Courier, command=lambda: changefont("Courier"))
-font.menu.add_checkbutton(label="Helvetica", variable=Helvetica, command=lambda: changefont("Helvetica"))
+#
+# # add a button to save the text
+# def saveas():
+#     global text
+#     t = text.get("1.0", "end-1c")
+#     savelocation = tkinter.filedialog.asksaveasfilename()
+#     file1 = open(savelocation, "w+")
+#     file1.write(t)
+#     file1.close()
+#
+#
+# button = Button(root, text="Save", command=saveas)
+# button.grid()
+#
+#
+# # add font changer
+# def changefont(font):
+#     global text
+#     text.config(font=font)
+#
+#
+# font = Menubutton(root, text="Font")
+# font.grid()
+# font.menu = Menu(font, tearoff=0)
+# font["menu"] = font.menu
+# Helvetica = IntVar()
+# Courier = IntVar()
+# font.menu.add_checkbutton(label="Courier", variable=Courier, command=lambda: changefont("Courier"))
+# font.menu.add_checkbutton(label="Helvetica", variable=Helvetica, command=lambda: changefont("Helvetica"))
 
 # todo colour
 # todo Menüleiste
